ee8559b779bed1867b2b6a26356b1c37504138d3
lucram <lucienramassamy@gmail.com>
2020-05-18 18:35:31 +0200

    integration de la home

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,25 @@
 </template>
 
 <style lang="scss">
+// IMPORT STYLE
+@import url('./style/reset.css');
 // IMPORT FONTS
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap');
 
 // GENERAL
 body {
+  height: 100vh;
+  width: 100%;
   font-family: 'Roboto', sans-serif;
   font-size: 13px;
   color: #44696c;
+  background-image: url('./assets/background.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 50%;
+  #app {
+    height: 100%;
+  }
   h1 {
     font-weight: 900;
     font-size: 18px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="home">
+    <img src="../assets/logo.png" alt="" srcset="">
+    <div class="wrapper--text">
+      <h1>
+        Vous avez toujours reve de devenir dresseur pokemon ? <br> Combatre la ligue ? <br> collectionné tous les pokemon ?
+        Aujourd'hui ces possible avec POKEMON GO
+      </h1>
+    </div>
   </div>
 </template>
 
@@ -14,4 +21,24 @@ export default {
 }
 </script>
 <style lang="scss">
+  .home {
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   padding: 30px;
+   padding-top: 50px;
+   .wrapper--text {
+     height: 300px;
+     padding: 10px;
+     padding-top: 30px;
+     background-color: #fffff5;
+     border-radius: 1%;
+     margin-top: 20px;
+     text-transform: uppercase;
+     opacity: 85%;
+     h1 {
+       line-height: 20px;
+     }
+   }
+  }
 </style>
