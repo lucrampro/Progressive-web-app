b4df0ff134b4b92ecea0834f0e0b2105b3af8860
lucram <lucienramassamy@gmail.com>
2020-05-20 03:05:25 +0200

    lol cesla dernier fois je fais une PWA

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,25 @@
 
 <script>
 // import library
-import { gsap } from 'gsap';
+import { gsap, Bounce } from 'gsap';
 
   export default {
     name: "app",
     mounted() {
       console.log(this.$route.name)
+      gsap.timeline()
+      .set(".overlay .logo", { scale: 0, y: "-50%", x: "-50%" })
+      .to(".overlay .logo", 1.5, { scale: 1, ease: Bounce.easeOut })
+      .to(".overlay .logo", 1, { rotate: "360deg", y: "-50%", x: "-50%" })
+      .to(".overlay .logo", 0.5, { scale: 0, ease: Bounce.easeOut })
+      .to(".overlay", 0.5, { opacity: 0 })
+      .set(".overlay", { height: 0 })
+      .staggerFrom(
+        ".home .logo, .home .wrapper--text",
+        0.5,
+        { opacity: 0, y: "50px" },
+        0.2
+      );
     },
     methods: {
   beforeEnter: function () {
@@ -30,11 +43,11 @@ import { gsap } from 'gsap';
   },
   enter: function (el, done) {
     let tl = gsap.timeline()
-
+    console.log('je suis le FUCKING ENTER')
     tl.staggerTo('body *', 1.5, {
       opacity: 1,
       onComplete: () => {
-        done();
+        done
       }
     })
 
@@ -44,7 +57,7 @@ import { gsap } from 'gsap';
     },
   leave: function (el, done) {
     let tl = gsap.timeline()
-
+    console.log('je suis le FUCKING LEAVE PTN')
     tl.staggerTo('body *', 1.5, {
       opacity: 0,
       onComplete: () => {
